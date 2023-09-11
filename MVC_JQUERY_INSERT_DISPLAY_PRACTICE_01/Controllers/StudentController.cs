42b51e0613462458b85d42af826d885d3e0b51d6
hash-amit <hash.amitkumar@gmail.com>
2023-09-11 23:42:44 +0530

    Add feature: Edit record

--- MVC_JQUERY_INSERT_DISPLAY_PRACTICE_01/Controllers/StudentController.cs
+++ MVC_JQUERY_INSERT_DISPLAY_PRACTICE_01/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -52,6 +53,34 @@ namespace MVC_JQUERY_INSERT_DISPLAY_PRACTICE_01.Controllers
             SqlCommand cmd = new SqlCommand("sp_StudentDelete", con);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@id", sid);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public JsonResult GetSelectedStudentRecord(int sid)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_GetOneRecord", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@sid", sid);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            string data = JsonConvert.SerializeObject(dt);
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public void StudentUpdate(string A, string B, string C, long D, int E)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("sp_StudentUpdate", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", A);
+            cmd.Parameters.AddWithValue("@email", B);
+            cmd.Parameters.AddWithValue("@pass", C);
+            cmd.Parameters.AddWithValue("@mobile", D);
+            cmd.Parameters.AddWithValue("@sid", E);
             cmd.ExecuteNonQuery();
             con.Close();
         }
